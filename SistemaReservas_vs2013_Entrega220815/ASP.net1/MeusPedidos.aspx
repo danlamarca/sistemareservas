505dc15c90924e6fa9042225f8f3377a88e6f705
Danilo - Moreira <danilo.lamarca@hotmail.com>
2017-09-02 09:43:43 -0300

    Add Newtonsoft

---
+++ SistemaReservas_vs2013_Entrega220815/ASP.net1/MeusPedidos.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/MasterPage.master" CodeFile="MeusPedidos.aspx.cs" Inherits="MeusPedidos" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MenuHorizontal_Menu" Runat="Server>    
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MenuHorizontal" Runat="Server">
+    
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <asp:Label id="txt_titulo" runat="server" text="Integração Meus Pedidos" />
+    </br>
+    <asp:CheckBox id="chk_im_cli" runat="server" text="Importar Cliente" checked="false" />&nbsp;
+    <asp:Button id="btn_importar" runat="server" text="OK" OnClick="btn_importar_Click" />
+
+</asp:Content>
---
+++ SistemaReservas_vs2013_Entrega220815/ASP.net1/MeusPedidos.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+using Library1.BO;
+using unirest_net.http;
+using unirest_net.request;
+
+public partial class MeusPedidos : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btn_importar_Click(object sender, EventArgs e)
+    {
+        //string teste = JsonConvert.DeserializeObject<Cliente>(variavelJson);
+
+        HttpResponse<string> jsonResponse = Unirest.post("http://httpbin.org/post")
+        .header("accept", "application/json")
+        .field("parameter", "value")
+        .field("foo", "bar")
+        .asJson<MyClass>();
+
+        string resposta  = jsonResponse.Body; 
+
+    }
+}
