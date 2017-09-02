a99a3f010b5eb98e112b3575e0b9878ee41ba9ab
Danilo - Moreira <danilo.lamarca@hotmail.com>
2017-09-02 13:28:12 -0300

    Unirest Biblioteca

---
+++ SistemaReservas_vs2013_Entrega220815/ASP.net1/MeusPedidos.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="MeusPedidos.aspx.cs" Inherits="MeusPedidos" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MenuHorizontal_Menu" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MenuHorizontal" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+</asp:Content>
+
---
+++ SistemaReservas_vs2013_Entrega220815/ASP.net1/MeusPedidos.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+using unirest_net.http;
+
+
+public partial class MeusPedidos : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        string json=String.Empty;
+
+        HttpResponse<string> jsonResponse = Unirest.post("http://httpbin.org/post")
+        .header("accept", "application/json")
+        .field("parameter", "value")
+        .field("foo", "bar")
+        .asJson<json>();
+
+        string retorno = jsonResponse.Body;
+        Dictionary<string,string> retorno2 = jsonResponse.Headers;
+    }
+}
