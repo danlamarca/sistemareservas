422f8aea91171b20fa380046ca6511eb485f4d35
Danilo - Micro de Desenv <lamarca.danilo@gmail.com>
2017-09-02 08:02:19 -0300

    PagSeguro modificações

--- SistemaReservas_vs2013_Entrega220815/ASP.net1/PagSeguro_Teste.aspx.cs
+++ SistemaReservas_vs2013_Entrega220815/ASP.net1/PagSeguro_Teste.aspx.cs
@@ -9,11 +9,14 @@ using System.Net;
 using System.Text;
 using System.Xml;
 using Library1;
+using Uol.PagSeguro;
 using Uol.PagSeguro.Constants;
 using Uol.PagSeguro.Domain;
 using Uol.PagSeguro.Service;
 using Uol.PagSeguro.Resources;
-
+using System.Web.UI;
+using Library1.BO;
+using System.Windows.Forms;
 
 public partial class PagSeguro_Teste : System.Web.UI.Page
 {
@@ -25,6 +28,26 @@ public partial class PagSeguro_Teste : System.Web.UI.Page
             //sessao que transitará por toda a aplicação, com o id da transação
             Session["sessionResponse"] = "";
             #endregion
+
+            #region javascript
+            //btn_pagseguro.Attributes.Add("onclick", "var texto = document.getElementById('lbl_teste'); texto.innerHTML = 'Treinamento!'; alert('oi')");
+            #endregion
+
+            Produto produto = new Produto();
+            produto.ID_PRODUTO = 1;
+            produto.NOME = "Vassoura";
+            produto.DESCRICAO = "Serviços Internos";
+            produto.VALOR = 50;
+
+            lbl_descricao.Text = produto.DESCRICAO;
+            lbl_id.Text = produto.ID_PRODUTO.ToString();
+            lbl_nome.Text = produto.NOME;
+            lbl_valorr.Text = produto.VALOR.ToString();
+
+            //setando os dados do form:
+
+            
+            
         }
     }
 
@@ -43,10 +66,10 @@ public partial class PagSeguro_Teste : System.Web.UI.Page
             using (WebClient client = new WebClient())
             {
                 NameValueCollection postData = new NameValueCollection()
-            {
-                {"email",email},
-                {"token",token}
-            };
+                {
+                    {"email",email},
+                    {"token",token}
+                };
 
                 string retorno = Encoding.UTF8.GetString(client.UploadValues(urlAddress, postData));
                 xmlRetorno.LoadXml(retorno);                
@@ -57,8 +80,13 @@ public partial class PagSeguro_Teste : System.Web.UI.Page
                 XmlNodeList elemento = xmlRetorno.GetElementsByTagName("id");
                 Session["sessionResponse"] = elemento[0].InnerXml;
 
-                //exemplo de carregamento da classe de Credenciais:
-                AccountCredentials credencias = new AccountCredentials(email, token); 
+                //exemplo de carregamento da classe de Credenciais do Vendedor:
+                AccountCredentials CREDENCIAIS = new AccountCredentials(email, token);
+                string ID_SESSAO = Session["sessionResponse"].ToString();
+                
+                //ver biblioteca PagSeguro js na MasterPage, segue chamada desta funcao obrigatoria:
+                //ScriptManager.RegisterClientScriptBlock(this, typeof(Page), "TScript", "PagSeguroDirectPayment.setSessionId('" + Session["sessionResponse"].ToString() + "'); var senderHash = PagSeguroDirectPayment.getSenderHash(); alert(senderHash)", true);
+               // ScriptManager.RegisterClientScriptBlock(this, typeof(Page), "TScript", "$("#getSenderHash").click(function(){var senderHash = PagSeguroDirectPayment.getSenderHash();console.log(senderHash);$("input#senderHash").val(senderHash);})", true);
             }
 
         }
@@ -72,4 +100,5 @@ public partial class PagSeguro_Teste : System.Web.UI.Page
     {
         Recupera_ID_Sessao();
     }
+ 
 }
